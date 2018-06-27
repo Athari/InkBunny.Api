3266cc6dd3eeb40d5e34b7786b881fd8217fc2e1
Athari <athari.p@gmail.com>
2018-06-27 12:51:37 +0300

    Init.

---
+++ Alba.InkBunny.Api/InkBunnyClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Alba.InkBunny.Api
+{
+    public class InkBunnyClient : IDisposable
+    {
+        public static readonly Uri BaseUri = new Uri("https://inkbunny.net/");
+
+        private string _userAgentName;
+        private string _userAgentVersion;
+        private string _sid;
+        private IWebProxy _proxy;
+
+        private readonly HttpClient _httpClient;
+
+        public InkBunnyClient()
+        {
+            var httpClientHandler = new HttpClientHandler {
+                AllowAutoRedirect = true,
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip,
+                Proxy = _proxy,
+                UseProxy = _proxy != null,
+            };
+            _httpClient = new HttpClient(httpClientHandler) {
+                BaseAddress = BaseUri,
+                DefaultRequestHeaders = {
+                    Referrer = BaseUri,
+                    UserAgent = {
+                        new ProductInfoHeaderValue(_userAgentName, _userAgentVersion),
+                        new ProductInfoHeaderValue("through Alba.InkBunny.Api"),
+                    }
+                }
+            };
+        }
+
+        private async Task<T> Request<T>(string api, IEnumerable<KeyValuePair<string, string>> arguments)
+        {
+            using (var request = new MultipartFormDataContent()) {
+                foreach (KeyValuePair<string, string> argument in arguments.Where(a => a.Value != null))
+                    request.Add(new StringContent(argument.Value), argument.Key);
+                using (HttpResponseMessage response = await _httpClient.PostAsync($"/api_{api}.php", request)) {
+                    response.EnsureSuccessStatusCode();
+                    return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+                }
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,40 @@
+<img align="right" width="128" src="Docs/Images/InkBunny256.png" style="margin: 0 20px">
+
+*Alba.InkBunny.Api: Cross-platform InkBunny API for .NET*
+=========================================================
+
+* [**GitHub repository**](https://github.com/Athari/InkBunny.Api)
+* [**NuGet package**](https://www.nuget.org/packages/Alba.InkBunny.Api)
+<!-- -->
+    PM> Install-Package Alba.InkBunny.Api
+
+Alba.InkBunny.Api is a library for accessing InkBunny.net website through official API.
+
+Getting started
+===============
+
+1. Install NuGet package [Alba.InkBunny.Api](https://www.nuget.org/packages/Alba.InkBunny.Api) using Package Manager:
+
+        PM> Install-Package Alba.InkBunny.Api
+
+    or .NET CLI:
+
+        > dotnet add package Alba.InkBunny.Api
+
+2. Add `using Alba.InkBunny.Api;` to your .cs file.
+
+3. TODO
+
+Framework Compatibility
+=======================
+
+TODO
+
+License
+=======
+Unlicensed under the [Unlicense](License.md).
+
+Links
+=====
+
+TODO
